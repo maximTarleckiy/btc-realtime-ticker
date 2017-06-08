58616bc58d03354e249def90721f73758f0d360a
maximTarleckiy <bbs_max@hotmail.com>
2017-06-08 14:05:58 +0300

    Rename Client to Ticker. Move some related logic to Ticker.js

--- public/Ticker.js
+++ public/Ticker.js
@@ -1,22 +1,40 @@
-function Client(ws_url, calculator) {
-    this.calculator = calculator;
-    this.conn = new WebSocket(ws_url);
+function Ticker(ws_url, calculator) {
+    var me = this;
 
-    this.conn.onopen = function(e) {
+    me._stop = false;
+    me.calculator = calculator;
+    me.conn = new WebSocket(ws_url);
+
+    me.conn.onopen = function(e) {
         console.log("Connection established!");
     };
 
-    this.conn.onmessage = Client.prototype.onmessage.bind(this);
+    me.conn.onmessage = Ticker.prototype.onmessage.bind(me);
+
+    me.interval = setInterval(
+        function() {
+            var res = calculator.calculate();
+            me.markTable(res['exchanges']);
+
+            document.getElementById('btc_usd').innerHTML = res['btc_usd']['mean'] || '---';
+            document.getElementById('euro_usd').innerHTML = res['euro_usd']['mean'] || '---';
+            document.getElementById('btc_euro').innerHTML = res['btc_euro']['mean'] || '---';
+        },
+        3000
+    );
 }
 
-Client.prototype.onmessage = function onmessage(e) {
+Ticker.prototype.onmessage = function onmessage(e) {
+    if (this._stop) {
+        return;
+    }
     var exchangeRate = JSON.parse(e.data);
     if (this.calculator.addExchangeRate(exchangeRate)) {
         this.addExchangeToDom(exchangeRate);
     }
 };
 
-Client.prototype.addExchangeToDom = function(exchangeRate) {
+Ticker.prototype.addExchangeToDom = function(exchangeRate) {
     var existingRow = document.getElementById(exchangeRate.provider_name);
     if (existingRow) {
         existingRow.remove()
@@ -46,7 +64,7 @@ Client.prototype.addExchangeToDom = function(exchangeRate) {
     table.insertBefore(row, table.firstChild);
 }
 
-Client.prototype.markTable = function(exchanges) {
+Ticker.prototype.markTable = function(exchanges) {
     exchanges.forEach(function(exchange) {
        if ('is_active' in exchange) {
            var row = document.getElementById(exchange.provider_name);
@@ -61,4 +79,9 @@ Client.prototype.markTable = function(exchanges) {
            }
        }
     });
+}
+
+Ticker.prototype.stop = function() {
+    this._stop = true;
+    clearInterval(this.interval);
 }
